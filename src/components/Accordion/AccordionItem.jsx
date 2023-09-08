0b2d625e00d6b2bdfb881229047a19bed4ea8bb7
shashiverma9 <www.jigarverma74@gmail.com>
2023-09-08 15:38:12 +0530

    add changes

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -9,6 +9,10 @@ const AccordionItem = ({ isOpen, title, toggleAccordion }) => {
       </div>
       {isOpen && (
         <div className="accordion-content">
+            <div className="APY-header">
+                <h3 className="toggle-heading">APY</h3>
+                <span className="price-tag">63.34%</span>
+            </div>
           <p className="label-text">. Calculated based on current rates</p>
           <p className="label-text">
             . All fugures are estimates provided for your convenience only, and
